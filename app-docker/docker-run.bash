45c1452d95215f448cf839c5aef121191880cb8f
Don Jacob <donjacob10@yahoo.com>
2018-12-13 21:33:09 -0800

    AMPLprojects Dockerfile ....

--- app-docker/docker-run.bash
+++ app-docker/docker-run.bash
@@ -5,6 +5,7 @@ tag="0.1"
 image_name="${name}:${tag}"
 container_name="ampl_container"
 
+docker images -a | grep $image_name | xargs docker rmi
 docker build -t $image_name .
 #docker ps -q --filter "name=${container_name}" | grep -q . && echo Found || echo Not Found
 docker ps -q --filter "name=${container_name}" | grep -q . && docker stop $container_name && docker rm -fv $container_name
